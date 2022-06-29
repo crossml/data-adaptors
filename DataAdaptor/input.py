d64138f4d27f6016ad3406c48587e82b4b54cb60
sourav <soura@crossml.com>
2022-06-29 13:26:38 +0530

    Input Adaptor

---
+++ DataAdaptor/input.py
@@ -0,0 +1,11 @@
+import config
+from botocore.exceptions import NoCredentialsError
+import os
+from config import extensions_list
+
+
+class Input:
+    def __init__(self,):
+        pass
+    def fileAdaptor(self,s3):
+        pass
