2a1d2d5cc43c29d4bd54d440e7a3468675bd52f0
sourav <soura@crossml.com>
2022-07-01 12:26:04 +0530

    input File extension validation

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -1,11 +1,17 @@
-import config
-from botocore.exceptions import NoCredentialsError
 import os
-from config import extensions_list
+from config import EXTENSION_LIST
 
 
-class Input:
+class InputAdaptor:
     def __init__(self,):
         pass
-    def fileAdaptor(self,s3):
-        pass
+
+    def fileAdaptor(local_file_path):
+        try:
+            extension = os.path.splitext(local_file_path)[-1].lower()
+            if extension in EXTENSION_LIST:
+                return ['valid File Extension']
+            else:
+                raise TypeError("invalid file extension")
+        except FileNotFoundError:
+            return False
