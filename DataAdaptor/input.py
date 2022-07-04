be3ca3459d881d6729424068c74b07ad878a5800
sourav <soura@crossml.com>
2022-07-04 13:52:57 +0530

    s3 upload code

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -1,17 +1,33 @@
+"""
+test
+"""
 import os
-from config import EXTENSION_LIST
-
+import boto3
+# from config import EXTENSION_LIST
+EXTENSION_LIST = ['.pdf', '.png', '.jpg', '.tif', '.jpeg']
 
 class InputAdaptor:
-    def __init__(self,):
+    """
+    Input Adaptor class
+    """
+
+    def __init__(self):
         pass
 
-    def fileAdaptor(self,local_file_path,cloud_name):
+    def file_adaptor(self,local_file_path, s3_bucket_name,s3_storage_file_path):
+        """
+        File Input Funtion
+        """
+
+        session = boto3.Session()
+        s3 = session.resource('s3')
         try:
             extension = os.path.splitext(local_file_path)[-1].lower()
             if extension in EXTENSION_LIST:
-                return (local_file_path,cloud_name)
-            else:
-                raise TypeError("invalid file extension")
-        except FileNotFoundError:   
-            return ("file not found")
+                bucket = s3_bucket_name
+                # print(local_file_path, cloud_name)
+                s3.meta.client.upload_file(local_file_path, bucket, s3_storage_file_path)
+                print("Upload Successful",s3_storage_file_path)
+        except FileNotFoundError:
+            print("file not found")
+        
