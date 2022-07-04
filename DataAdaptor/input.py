ba125363cb6a5acfff78ecede1e84636a2446851
sourav <soura@crossml.com>
2022-07-04 16:10:59 +0530

    code formating

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -1,9 +1,9 @@
 """
-test
+Input File
 """
 import os
 import boto3
-from config import EXTENSION_LIST
+from .config import EXTENSION_LIST
 
 class InputAdaptor:
     """
@@ -13,19 +13,17 @@ class InputAdaptor:
     def __init__(self):
         pass
 
-    def file_adaptor(self,local_file_path, s3_bucket_name,s3_storage_file_path):
+    def file_adaptor(self,local_file_path, s3_bucket_name,s3_file_storage_path):
         """
         File Input Funtion
         """
-
         session = boto3.Session()
         s3 = session.resource('s3')
         try:
             extension = os.path.splitext(local_file_path)[-1].lower()
             if extension in EXTENSION_LIST:
-                bucket = s3_bucket_name
-                s3.meta.client.upload_file(local_file_path, bucket, s3_storage_file_path)
-                print("Upload Successful",s3_storage_file_path)
+                s3.meta.client.upload_file(local_file_path, s3_bucket_name, s3_file_storage_path)
+                print("Upload Successful",s3_file_storage_path)
         except FileNotFoundError:
             print("file not found")
         
