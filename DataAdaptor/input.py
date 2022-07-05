067566a7b1943685cb6b545b91c33051f4fc34d6
sourav <soura@crossml.com>
2022-07-05 17:48:12 +0530

    added cloud condition

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -5,6 +5,7 @@ import os
 import boto3
 from .config import EXTENSION_LIST
 
+
 class InputAdaptor:
     """
     Input Adaptor class
@@ -13,7 +14,7 @@ class InputAdaptor:
     def __init__(self):
         pass
 
-    def file_adaptor(self,local_file_path, s3_bucket_name,s3_file_storage_path):
+    def file_adaptor(self, local_file_path, s3_bucket_name, s3_file_storage_path, colud_name):
         """
         File Input Funtion
         """
@@ -22,8 +23,12 @@ class InputAdaptor:
         try:
             extension = os.path.splitext(local_file_path)[-1].lower()
             if extension in EXTENSION_LIST:
-                s3.meta.client.upload_file(local_file_path, s3_bucket_name, s3_file_storage_path)
-                print("Upload Successful",s3_file_storage_path)
+                if colud_name.lower() == 'aws':
+                    bucket = s3_bucket_name
+                    s3.meta.client.upload_file(
+                        local_file_path, bucket, s3_file_storage_path)
+                    print("Upload Successful", s3_file_storage_path)
+                else:
+                    print('invalid cloud option')
         except FileNotFoundError:
             print("file not found")
-        
