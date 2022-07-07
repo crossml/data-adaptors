f12d328730cc6d99cc2e6e8f13b594fce9adc820
sourav <soura@crossml.com>
2022-07-07 19:03:44 +0530

    feat: url file adaptor
    The url file adaptor will take url as input and after validating the
    url, it will upload the file object of the given url to selected
     cloud storage platform.

---
+++ DataAdaptor/input.py
@@ -0,0 +1,32 @@
+"""
+Input File
+"""
+
+from urllib.parse import urlparse
+import requests
+
+
+class InputAdaptor:
+    """
+    Input Adaptor class
+    """
+
+    def url_upload(self, link, colud_name):
+        """
+        url file upload Function
+        """
+        try:
+            url_object = requests.get(link, stream=True).raw
+            file_name = os.path.basename(urlparse(link).path)
+            extension = os.path.splitext(file_name)[-1].lower()
+            if extension in EXTENSION_LIST:
+                if colud_name.lower() == 'aws':
+                    S3.meta.client.upload_fileobj(
+                        url_object, S3_BUCKET_NAME, INPUT_FILE_FOLDER + file_name)
+                    print("Upload Successful", INPUT_FILE_FOLDER + file_name)
+                else:
+                    print('invalid cloud option')
+            else:
+                print("invalid url")
+        except IOError:
+            print("url not found")
