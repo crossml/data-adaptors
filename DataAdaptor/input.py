a15d8821bbb377c7bae52125d9c0939eb93c0a1a
sourav <soura@crossml.com>
2022-07-11 15:26:05 +0530

    feat: ftp file adaptor
    Added a file adaptor function for input class which will connect with
     ftp server fetch the data from given folder and save it into tmp folder
    after validating it's extension then save put he valid files to s3 if
     already doesn't exist.

---
+++ DataAdaptor/input.py
@@ -0,0 +1,53 @@
+"""
+Input File
+"""
+
+import ftplib
+from contextlib import closing
+
+
+class InputAdaptor:
+    """
+    Input Adaptor class
+    """
+
+    def __init__(self):
+        pass
+
+
+def ftp_upload(ftp_host, username, password, ftp_folder_path):
+    """
+    ftp file upload function
+    """
+    try:
+        with closing(ftplib.FTP()) as ftp:
+            # connect to the FTP server
+            ftp.connect(ftp_host, 21, 30*5)  # 5 mins timeout
+            ftp.login(username, password)
+            ftp.set_pasv(True)
+
+            # get filenames within the directory
+            filenames = ftp.nlst(ftp_folder_path)
+            lst = []
+            for filename in filenames:
+                # extrating particular filename from filenames
+                tmp_path = os.path.join('/tmp', os.path.basename(filename))
+                extension = os.path.splitext(tmp_path)[-1].lower()
+                if extension in EXTENSION_LIST:
+                    # check if valid allowed extension
+                    if not os.path.exists(tmp_path):
+                        # check if file already exist in tmp folder
+                        with open(tmp_path, 'wb') as f:
+                            # writing file at /tmp folder
+                            ftp.retrbinary('RETR %s' % filename, f.write)
+
+                            # calling function to save data to s3
+                            s3_file = upload_file_to_s3(tmp_path)
+
+                            # appending s3's response to list
+                            lst.append(s3_file)
+                    else:
+                        print("file already exist in tmp folder ")
+            print(lst)
+    except Exception as error:
+        print(error)
