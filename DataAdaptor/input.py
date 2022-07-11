1ab410e82983cc63ab604bd7799cd39b3ca86376
sourav <soura@crossml.com>
2022-07-11 15:16:50 +0530

    feat: validation on file saving on s3
    Added validation on file saving on s3 to
     prevent saving of duplicate files.

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -15,8 +15,23 @@ def upload_file_to_s3(local_file_path):
     """
     Upload File to s3
     """
-    S3.meta.client.upload_file(
-        local_file_path, S3_BUCKET_NAME, INPUT_FILE_FOLDER+local_file_path)
+    try:
+        # checking if file is already exist in s3
+        res = S3.meta.client.list_objects_v2(Bucket=S3_BUCKET_NAME,
+                                             Prefix=INPUT_FILE_FOLDER +
+                                             os.path.basename(local_file_path),
+                                             MaxKeys=1)
+        if ('Contents' in res) is True:
+            print("file already exist in s3")
+        else:
+            # saving file to s3
+            S3.meta.client.upload_file(
+                local_file_path, S3_BUCKET_NAME, 'Temp/' + os.path.basename(local_file_path))
+            print("Upload Successful", 'Temp/' +
+                  os.path.basename(local_file_path))
+            return 'Temp/' + os.path.basename(local_file_path)
+    except Exception as error:
+        print(error)
 
 
 class InputAdaptor:
