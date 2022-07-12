aa8236220485edf905312bef32a74b6378c76c8c
sourav <soura@crossml.com>
2022-07-12 18:00:43 +0530

    feat: return statement
    Added return statement in file adaptor and s3 saving function.

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -23,13 +23,14 @@ def upload_file_to_s3(local_file_path):
                                              MaxKeys=1)
         if ('Contents' in res) is True:
             print("file already exist in s3")
+            return False
         else:
             # saving file to s3
             S3.meta.client.upload_file(
-                local_file_path, S3_BUCKET_NAME, 'Temp/' + os.path.basename(local_file_path))
-            print("Upload Successful", 'Temp/' +
+                local_file_path, S3_BUCKET_NAME,INPUT_FILE_FOLDER+ os.path.basename(local_file_path))
+            print("Upload Successful", INPUT_FILE_FOLDER +
                   os.path.basename(local_file_path))
-            return 'Temp/' + os.path.basename(local_file_path)
+            return INPUT_FILE_FOLDER + os.path.basename(local_file_path)
     except Exception as error:
         print(error)
 
@@ -42,17 +43,18 @@ class InputAdaptor:
     def __init__(self):
         pass
 
-    def file_upload(self, local_file_path, colud_name):
+    def file_upload(self, local_file_path, cloud_name):
         """
         File Input Funtion
         """
         try:
             extension = os.path.splitext(local_file_path)[-1].lower()
             if extension in EXTENSION_LIST:
-                if colud_name.lower() == 'aws':
+                if cloud_name.lower() == 'aws':
                     upload_file_to_s3(local_file_path)
                     print("Upload Successful",
                           INPUT_FILE_FOLDER+local_file_path)
+                    return INPUT_FILE_FOLDER+local_file_path
                 else:
                     raise Exception("Sorry, invalid cloud ")
         except FileNotFoundError:
