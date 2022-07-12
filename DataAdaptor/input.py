245e438496397fd34882920dab922bda1cde9547
sourav <soura@crossml.com>
2022-07-12 18:06:27 +0530

    feat: return statement
    Added return statement in ftp function

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -14,40 +14,48 @@ class InputAdaptor:
     def __init__(self):
         pass
 
+    def ftp_upload(self, ftp_host, username, password, ftp_folder_path, cloud_name):
+        """
+        ftp file upload function
+        """
+        try:
+            with closing(ftplib.FTP()) as ftp:
+                # connect to the FTP server
+                ftp.connect(ftp_host, 21, 30*5)  # 5 mins timeout
+                ftp.login(username, password)
+                ftp.set_pasv(True)
 
-def ftp_upload(ftp_host, username, password, ftp_folder_path):
-    """
-    ftp file upload function
-    """
-    try:
-        with closing(ftplib.FTP()) as ftp:
-            # connect to the FTP server
-            ftp.connect(ftp_host, 21, 30*5)  # 5 mins timeout
-            ftp.login(username, password)
-            ftp.set_pasv(True)
+                # get filenames within the directory
+                filenames = ftp.nlst(ftp_folder_path)
+                lst = []
+                for filename in filenames:
+                    # extrating particular filename from filenames
+                    tmp_path = os.path.join('/tmp', os.path.basename(filename))
+                    extension = os.path.splitext(tmp_path)[-1].lower()
+                    if extension in EXTENSION_LIST:
+                        if cloud_name.lower() == 'aws':
 
-            # get filenames within the directory
-            filenames = ftp.nlst(ftp_folder_path)
-            lst = []
-            for filename in filenames:
-                # extrating particular filename from filenames
-                tmp_path = os.path.join('/tmp', os.path.basename(filename))
-                extension = os.path.splitext(tmp_path)[-1].lower()
-                if extension in EXTENSION_LIST:
-                    # check if valid allowed extension
-                    if not os.path.exists(tmp_path):
-                        # check if file already exist in tmp folder
-                        with open(tmp_path, 'wb') as f:
-                            # writing file at /tmp folder
-                            ftp.retrbinary('RETR %s' % filename, f.write)
+                            # check if valid allowed extension
+                            if not os.path.exists(tmp_path):
+                                # check if file already exist in tmp folder
+                                with open(tmp_path, 'wb') as f:
+                                    # writing file at /tmp folder
+                                    ftp.retrbinary('RETR %s' %
+                                                   filename, f.write)
 
-                            # calling function to save data to s3
-                            s3_file = upload_file_to_s3(tmp_path)
+                                    # calling function to save data to s3
+                                    s3_file = upload_file_to_s3(tmp_path)
+                                    lst.append(s3_file)
 
-                            # appending s3's response to list
-                            lst.append(s3_file)
+                                    # appending s3's response to list
+                            else:
+                                print("file already exist in tmp folder ")
+
+                        else:
+                            raise Exception("Sorry, invalid cloud ")
                     else:
-                        print("file already exist in tmp folder ")
+                        raise Exception("Sorry, file format")
+            return lst
             print(lst)
-    except Exception as error:
-        print(error)
+        except Exception as error:
+            print(error)
