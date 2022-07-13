d79ae5bfdd27d78f03f756373249f0ecff23ad90
sourav <soura@crossml.com>
2022-07-13 11:12:57 +0530

    feat: restructuring code
    Change the position of if block for cloud type validation.

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -33,26 +33,22 @@ class InputAdaptor:
                     tmp_path = os.path.join('/tmp', os.path.basename(filename))
                     extension = os.path.splitext(tmp_path)[-1].lower()
                     if extension in EXTENSION_LIST:
-                        if cloud_name.lower() == 'aws':
-
-                            # check if valid allowed extension
-                            if not os.path.exists(tmp_path):
-                                # check if file already exist in tmp folder
-                                with open(tmp_path, 'wb') as f:
-                                    # writing file at /tmp folder
-                                    ftp.retrbinary('RETR %s' %
-                                                   filename, f.write)
-
+                        # check if valid allowed extension
+                        if not os.path.exists(tmp_path):
+                            # check if file already exist in tmp folder
+                            with open(tmp_path, 'wb') as f:
+                                # writing file at /tmp folder
+                                ftp.retrbinary('RETR %s' %
+                                               filename, f.write)
+                                if cloud_name.lower() == 'aws':
                                     # calling function to save data to s3
                                     s3_file = upload_file_to_s3(tmp_path)
                                     lst.append(s3_file)
-
-                                    # appending s3's response to list
-                            else:
-                                print("file already exist in tmp folder ")
-
+                                else:
+                                    raise Exception("Sorry, invalid cloud ")
+                                # appending s3's response to list
                         else:
-                            raise Exception("Sorry, invalid cloud ")
+                            print("file already exist in tmp folder ")
                     else:
                         raise Exception("Sorry, file format")
             return lst
