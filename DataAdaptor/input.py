e25f5140e03211b098a5f0cf2cd16c6e5839f74b
sourav <soura@crossml.com>
2022-07-13 17:41:26 +0530

    feat: comments
    Added proper comments.

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -15,13 +15,18 @@ class InputAdaptor:
         '''
         try:
             if zipfile.is_zipfile(zip_file):
+                # checking given file is zip
                 with zipfile.ZipFile(zip_file, "r") as zip_file_name:
+                    # reading zip file
                     for file_name in zip_file_name.namelist():
+                        # extracting files from zip
                         extension = os.path.splitext(file_name)[-1].lower()
                         if extension not in EXTENSION_LIST:
+                            # checking if any invalid extension file in present in zip file
                             raise Exception("Sorry, invalid zip file")
 
                 if cloud_name.lower() == 'aws':
+                    #checking cloud name
                     upload_file_to_s3(zip_file)                    
                     return INPUT_FILE_FOLDER+zip_file
 
