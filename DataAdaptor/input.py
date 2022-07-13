6df86848d4fb7854a4f8c2c7d9a99d551a0219c8
sourav <soura@crossml.com>
2022-07-13 17:40:27 +0530

    feat: comments
    Added proper comments.

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -16,13 +16,17 @@ class InputAdaptor:
         url file upload Function
         """
         try:
+            # saving file in object from url
             url_object = requests.get(link, stream=True).raw
             file_name = os.path.basename(urlparse(link).path)
+            # extracting file extension
             extension = os.path.splitext(file_name)[-1].lower()
             if extension in EXTENSION_LIST:
+                # check file extension is valid from specified extension list
                 if colud_name.lower() == 'aws':
+                    # checking cloud name
                     S3.meta.client.upload_fileobj(
-                        url_object, S3_BUCKET_NAME, INPUT_FILE_FOLDER + file_name)
+                        url_object, S3_BUCKET_NAME, INPUT_FILE_FOLDER + file_name)  # saving data to s3
                     return INPUT_FILE_FOLDER+file_name
                 else:
                     return ('invalid cloud option')
