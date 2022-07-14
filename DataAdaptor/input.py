b1bc45cc6ba087f6b45fd3aec3f6746460215caf
sourav-crossml <88368353+sourav-crossml@users.noreply.github.com>
2022-07-14 17:04:03 +0530

    feat: url file adaptor (#6)
    * feat: url file adaptor
    
    The url file adaptor will take url as input and after validating the
    url, it will upload the file object of the given url to selected
     cloud storage platform.
    
    * feat: return statement
    
    added return statement in url function
    
    * feat: return statements
    
    removed unwanted print and added return statements
    
    * feat: config file
    
    Removed config file
    
    * feat: comments
    
    Added proper comments.
    
    * feat: test cases
    
    Added unit test case file.
    
    * feat: static path
    
    Removed static paths from test file and added proper naming conventions.
    
    * feat: static path
    
    Removed static paths from test file.
    
    Co-authored-by: sourav <soura@crossml.com>
    Co-authored-by: Payal Jhamta <88313683+payal-crossml@users.noreply.github.com>

--- DataAdaptor/input.py
+++ DataAdaptor/input.py
@@ -1,6 +1,8 @@
 """
 Input File
 """
+from urllib.parse import urlparse
+import requests
 import os
 import boto3
 from config import EXTENSION_LIST
@@ -24,14 +26,39 @@ def upload_file_to_s3(local_file_path):
         return error
 
 
+
 class InputAdaptor:
     """
     Input Adaptor class
     """
-
     def __init__(self):
         pass
 
+    def url_upload(self, link, colud_name):
+        """
+        url file upload Function
+        """
+        try:
+            # saving file in object from url
+            url_object = requests.get(link, stream=True).raw
+            file_name = os.path.basename(urlparse(link).path)
+            # extracting file extension
+            extension = os.path.splitext(file_name)[-1].lower()
+            if extension in EXTENSION_LIST:
+                # check file extension is valid from specified extension list
+                if colud_name.lower() == 'aws':
+                    # checking cloud name
+                    S3.meta.client.upload_fileobj(
+                        url_object, S3_BUCKET_NAME, INPUT_FILE_FOLDER + file_name)  # saving data to s3
+                    return INPUT_FILE_FOLDER+file_name
+                else:
+                    return ('invalid cloud option')
+            else:
+                return ("invalid url")
+        except IOError:
+            return ("url not found")
+
+    
     def file_upload(self, local_file_path, cloud_name):
         """
         File Input Funtion
--- DataAdaptor/test.py
+++ DataAdaptor/test.py
@@ -1,12 +1,24 @@
 # unit test case
 import unittest
-from test import InputAdaptor
+from input import InputAdaptor
+
 
 
 class TestInputMethods(unittest.TestCase):
     """
     test class
     """
+
+
+    def url_method(self):
+        """
+        url test function
+        """
+        input_values = InputAdaptor()
+        inputfunc = input_values.url_upload(
+            '', '')  # enter url and cloud name
+        # enter output file name for comparision
+        output_values = ""
 
     def file_method(self):
         """
@@ -15,6 +27,7 @@ class TestInputMethods(unittest.TestCase):
         inp = InputAdaptor()
         inputfunc = inp.file_upload('',  '')  # enter file and cloud name
         output_values = ""  # enter output file name for comparision
+
         # assertEqual() to check true of test value
         self.assertEqual(inputfunc, output_values)
 
