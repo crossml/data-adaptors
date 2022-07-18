130127a649a120b6f2c29bf685a53e9a072e2bb6
sourav <soura@crossml.com>
2022-07-18 16:22:44 +0530

    feat: pip package
    Converted Input Adaptor code to a pip package.

--- DataAdaptor/test.py
+++ DataAdaptor/test.py
@@ -8,6 +8,39 @@ class TestInputMethods(unittest.TestCase):
     test class
     """
 
+    def file_method(self):
+        """
+        file test function
+        """
+        inp = InputAdaptor()
+        inputfunc = inp.file_upload('',  '')  # enter file and cloud name
+        output_values = ""  # enter output file name for comparision
+        # assertEqual() to check true of test value
+        self.assertEqual(inputfunc, output_values)
+
+    def url_method(self):
+        """
+        url test function
+        """
+        input_values = InputAdaptor()
+        inputfunc = input_values.url_upload(
+            '', '')  # enter url and cloud name
+        # enter output file name for comparision
+        output_values = ""
+        # assertEqual() to check true of test value
+        self.assertEqual(inputfunc, output_values)
+
+    def zip_method(self):
+        """
+        zip test function
+        """
+        inp = InputAdaptor()
+        inputfunc = inp.zip_upload('', '')  # enter zip file and cloud name
+        # enter output file name for comparision
+        output_values = ""
+        # assertEqual() to check true of test value
+        self.assertEqual(inputfunc, output_values)
+
     def ftp_method(self):
         """
         ftp test function
